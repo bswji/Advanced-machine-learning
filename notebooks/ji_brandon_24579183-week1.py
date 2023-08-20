6ec947604ab7b68bf558f33c2bc2a2ed6534fb39
bswji <104555111+bswji@users.noreply.github.com>
2023-08-20 23:37:49 +1000

    a

--- notebooks/ji_brandon_24579183-week1.py
+++ notebooks/ji_brandon_24579183-week1.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
 #Read data
-data = pd.read_csv("Data/Week 1/train.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/bswji/Advanced-machine-learning/main/Data/Week%201/train.csv?token=GHSAT0AAAAAACGNNF36GLG4QQMGBEX2ALZIZG6ESAQ")
+
+data = pd.DataFrame(data)
+data.info()
+
+#Clean data
+#Remove player id
+data = data.drop(columns = "player_id")
+data.info()
+#Check for missing values
+data.isnull().sum()
+len(data)

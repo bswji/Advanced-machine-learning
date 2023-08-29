32cf79d46609bf641e8ca142825c4c59ba893242
bswji <104555111+bswji@users.noreply.github.com>
2023-08-30 00:15:30 +1000

    Update

--- notebooks/ji_brandon_24579183-week2.py
+++ notebooks/ji_brandon_24579183-week2.py
@@ -3,4 +3,25 @@ import pandas as pd
 pd.set_option("display.max_columns" , None)
 
 #Read data
-data = pd.read_csv("https://raw.githubusercontent.com/bswji/Advanced-machine-learning/main/Data/Week%201/train.csv?token=GHSAT0AAAAAACGH327SAEMLUN76VMNLCSZUZHJ24MQ")
+data = pd.read_csv('Data/Week 1/train.csv')
+
+data.info()
+
+#Remove player_id, num, type
+data['type'].value_counts()
+data.drop(columns = ['type','player_id','num'], inplace=True)
+data.info()
+
+#Check columns with missing values
+missing = data.isna().sum()
+missing.to_csv('missing')
+
+#Rec rank has 39055 missing values, dunk ratio has 30793, pick 54705 -Way too many missing values. Decided to remove.
+data.drop(columns = 'pick', inplace = True)
+data.drop(columns = 'Rec_Rank', inplace = True)
+data.drop(columns = 'dunks_ratio', inplace = True)
+data.info()
+
+#Check how many have missing values in rest of columns
+data.isna().sum()
+len(data)

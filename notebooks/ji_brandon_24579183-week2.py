d81e234bcc8597986d7e19354b4954e4dc1bc2a8
bswji <104555111+bswji@users.noreply.github.com>
2023-08-29 14:49:11 +1000

    Week 2

---
+++ notebooks/ji_brandon_24579183-week2.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+pd.set_option("display.max_columns" , None)
+
+#Read data
+data = pd.read_csv("https://raw.githubusercontent.com/bswji/Advanced-machine-learning/main/Data/Week%201/train.csv?token=GHSAT0AAAAAACGH327SAEMLUN76VMNLCSZUZHJ24MQ")
